0ebf966232251dd6cb5bc407edfba9090dfff0d9
DACUNHA Alexis <alexis.dacunha@vinci-construction.com>
2018-06-18 17:45:02 +0200

    gitignore

---
+++ CDbVisualizerSAMP.cpp
@@ -0,0 +1,6 @@
+#include "CDbVisualizerSAMP.h"
+
+CDbVisualizerSAMP::CDbVisualizerSAMP()
+{
+
+}
---
+++ CDbVisualizerSAMP.h
@@ -0,0 +1,11 @@
+#ifndef CDBVISUALIZERSAMP_H
+#define CDBVISUALIZERSAMP_H
+
+
+class CDbVisualizerSAMP
+{
+public:
+    CDbVisualizerSAMP();
+};
+
+#endif // CDBVISUALIZERSAMP_H
